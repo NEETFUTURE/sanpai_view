da382674a3df9c465ea6970ed09536e629c79ed8
NEETFUTURE <marumarumaru62@gmail.com>
2015-08-17 20:47:54 +0900

    評価によるソートを実装

--- waterduct.py
+++ waterduct.py
@@ -1,8 +1,7 @@
 # -*- coding: utf_8 -*-
-import requests
+from urllib import request
 import sys
 import lxml.html
-import sqlite3
 import os.path
 import pickle
 
@@ -17,23 +16,23 @@ DATABASE = os.path.join(os.path.curdir, "data.pickle")
 gData = None
 
 def getlist(num):
-    r=requests.get(URL+"/php/waterducts/imta/?log=%d"%(num))
-    root = lxml.html.fromstring(r.text)
+    r=request.urlopen(URL+"/php/waterducts/imta/?log=%d"%(num))
+    root = lxml.html.fromstring(r.read().decode("cp932"))
 
     a = root.xpath("//td[@class='title']")
-    sclist = [dict(id       = b.xpath("../td")[0].text.strip(),\
-                   title    = b.xpath("a")[0].text.strip(),\
-                   url      = URL+PHPU+b.xpath("a")[0].attrib['href'].strip()[1:],\
-                   author   = b.xpath("../td")[2].text.strip(),\
-                   hyoka    = b.xpath("../td")[5].text.strip(),\
-                   point    = b.xpath("../td")[6].text.strip(),\
-                   rate     = b.xpath("../td")[7].text.strip(),\
-                   size     = b.xpath("../td")[8].text.strip(),\
+    sclist = [dict(id = b.xpath("../td")[0].text.strip(),\
+                   title = b.xpath("a")[0].text.strip(),\
+                   url = URL+PHPU+b.xpath("a")[0].attrib['href'].strip()[1:],\
+                   author = b.xpath("../td")[2].text.strip(),\
+                   hyoka = b.xpath("../td")[5].text.strip(),\
+                   point = b.xpath("../td")[6].text.strip(),\
+                   rate = b.xpath("../td")[7].text.strip(),\
+                   size = b.xpath("../td")[8].text.strip(),\
                    ) 
                    for b in a]
     sclist = [[b, dict(hyoka = float(Fraction_zero(b["hyoka"])),
                        point = int(b["point"]),
-                       rate  = float(b["rate"]),
+                       rate = float(b["rate"]),
                        size = float(b["size"].rstrip("KB "))
                        )
              ] for b in sclist]
@@ -41,8 +40,8 @@ def getlist(num):
     return sclist
 
 def getclists():
-    top_page_ct = requests.get(URL + "/php/waterducts/imta")
-    ro = lxml.html.fromstring(top_page_ct.text)
+    tpc = request.urlopen(URL + "/php/waterducts/imta")
+    ro = lxml.html.fromstring(tpc.read().decode("cp932"))
     links = ro.xpath("//div[@class='page']/ul/li")
 
     print("リンクの数: %d"%(len(links)))
@@ -59,9 +58,10 @@ def getclists():
 def base():
     return template('templates/cate.html', clists = gData)
 
+@route('/sort_hyoka')
 def sort_hyoka():
-    cli = sorted(gData, key=lambda x: x[1]["hyoka"])
-    return template('templates/cate.html', clists = gData)
+    cli = sorted(gData, key=lambda x: x[1]["hyoka"], reverse=True)
+    return template('templates/cate.html', clists = cli)
 
 def Fraction_zero(n):
     try:
